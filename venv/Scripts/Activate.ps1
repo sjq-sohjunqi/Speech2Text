a09e6f91318f145bd5996ef92abd0172dac2385a
sjq-sohjunqi <65146620+sjq-sohjunqi@users.noreply.github.com>
2020-05-11 14:09:51 +0800

    Updated to work with flask
    Flask unable to run after changing directories but now working

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\cygwin64\home\Soh Jun Qi\S2T\venv"
+$env:VIRTUAL_ENV="C:\cygwin64\home\Soh Jun Qi\S2T\Speech2Text\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\cygwin64\home\Soh Jun Qi\S2T\venv"
+set "VIRTUAL_ENV=C:\cygwin64\home\Soh Jun Qi\S2T\Speech2Text\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
