1dd935076573162d7779f095f11675d3a65b5010
sjq-sohjunqi <65146620+sjq-sohjunqi@users.noreply.github.com>
2020-05-12 20:36:11 +0800

    Experimenting with forms

--- S2T/__init__.py
+++ S2T/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from config import Config
 
 S2T = Flask(__name__)
-app.config.from_object(Config)
+S2T.config.from_object(Config)
 
 from S2T import routes
--- S2T/routes.py
+++ S2T/routes.py
@@ -1,8 +1,18 @@
 from flask import render_template
 from S2T import S2T
+from S2T.forms import LoginForm
 
 @S2T.route('/')
 @S2T.route('/index')
 def index():
 	user = {'username': 'Miguel'}
 	return render_template('index.html', title='Home', user=user)
+
+@S2T.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(
+			form.username.data, form.remember_me.data))
+		return redirect('/index')
+	return render_template('login.html', title='Sign In', form=form)
