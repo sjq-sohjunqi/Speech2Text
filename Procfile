d151d4dfc8bd403fde7278f77ee660048f52f8d8
sjq-sohjunqi <65146620+sjq-sohjunqi@users.noreply.github.com>
2020-07-25 14:29:24 +0800

    Heroku deployment [TEST]

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn S2T:S2T
--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 class Config(object):
 	SECRET_KEY = '\xf2\xa1/\x9e\xf2B\xc5N\x95\xd2\x8ey\x00a\xc2\x93C\x01$\x16\x92\xf69\xfd'
-	SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1:3306/speech2text'
+	# SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1:3306/speech2text'
+	SQLALCHEMY_DATABASE_URI = os.getenv("DB_CONN")
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	
 	os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'teak-inquiry-279207-d295e37d764e.json'
